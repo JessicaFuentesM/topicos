4d8352a95169cfba24db2efde4143fd13cedfbb0
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2022-07-01 17:04:26 -0500

    SPA1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    HeroesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/heroe.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class HeroeService {
+
+	constructor() {
+		console.log("servicio Activo");
+
+	}
+}
